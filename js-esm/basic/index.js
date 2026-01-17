34b8718ef9d62ad6dc5b348ae33b4c3bbe47706f
Marko K. S. <marko.seppanen@gmail.com>
2026-01-18 00:08:41 +0100

    Fixes

--- js-esm/basic/index.js
+++ js-esm/basic/index.js
@@ -3,26 +3,26 @@
     file then prints its contents using ES Modules.
 */
 
-import YINI from 'yini-parser';
+import YINI from "yini-parser"
 
 try {
     // Parse the YINI config file.
-    const config = YINI.parseFile('./config.yini');
+    const config = YINI.parseFile("./config.yini")
 
     // If you want to parse the file in strict mode, use this line instead:
     // const config = YINI.parseFile(file, true);
 
     // Print some value in the config.
-    console.log('App Name  = ' + config.App.name)
-    console.log('isCaching = ' + config.App.Special.isCaching)
+    console.log("App Name  = " + config.App.name)
+    console.log("isCaching = " + config.App.Special.isCaching)
     console.log()
 
     // Print the result.
-    console.log('Parsed config:');
+    console.log("Parsed config:")
     console.log(config)
 
-    console.log('Parsed config as JSON:');
-    console.log(JSON.stringify(config, null, 2));
+    console.log("Parsed config as JSON:")
+    console.log(JSON.stringify(config, null, 2))
 } catch (error) {
-    console.error('Error parsing config.yini:', error.message);
+    console.error("Error parsing config.yini:", error.message)
 }
